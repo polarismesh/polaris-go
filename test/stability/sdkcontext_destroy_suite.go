da504302291962989fb3ce591f6a631bc94e0dab
andrewshan <andrewshan@tencent.com>
2021-09-18 10:47:24 +0800

    fix: remove print of statck

--- test/stability/sdkcontext_destroy_suite.go
+++ test/stability/sdkcontext_destroy_suite.go
@@ -18,7 +18,6 @@
 package stability
 
 import (
-	"fmt"
 	"github.com/polarismesh/polaris-go/api"
 	plog "github.com/polarismesh/polaris-go/pkg/log"
 	"github.com/polarismesh/polaris-go/test/util"
@@ -57,7 +56,6 @@ const lumberJackPrefix = "github.com/natefinch/lumberjack.(*Logger).millRun"
 
 //从调用栈解析routines数量，返回普通routines数量，lumberjack数量
 func parseRoutines(stack string) (int, int) {
-	fmt.Printf("%s", stack)
 	var normalCount int
 	var lumberjackCount int
 	lines := strings.Split(stack, "\n\n")
@@ -88,16 +86,16 @@ func (s *SDKContextDestroySuite) TestConsumerDestroy(c *check.C) {
 	var preCreateRoutinesNum int
 	routinesCount, _ = parseRoutines(getAllStack())
 	preCreateRoutinesNum = routinesCount
-	//log.Printf("preCreateRoutinesNum %v", preCreateRoutinesNum)
+	log.Printf("preCreateRoutinesNum %v", preCreateRoutinesNum)
 	consumer, err := api.NewConsumerAPIByConfig(configuration)
 	c.Assert(err, check.IsNil)
-	//log.Printf("consumerAPI runningRoutinesNum %v", runtime.NumGoroutine())
+	log.Printf("consumerAPI runningRoutinesNum %v", runtime.NumGoroutine())
 	consumer.Destroy()
 	time.Sleep(5 * time.Second)
 	var lumberjackCount int
 	routinesCount, lumberjackCount = parseRoutines(getAllStack())
 	afterDestroyRoutinesNum := routinesCount
-	//log.Printf("afterDestroyRoutinesNum %v", afterDestroyRoutinesNum)
+	log.Printf("afterDestroyRoutinesNum %v", afterDestroyRoutinesNum)
 	c.Assert(preCreateRoutinesNum, check.Equals, afterDestroyRoutinesNum)
 	c.Assert(lumberjackCount < plog.MaxLogger, check.Equals, true)
 }
@@ -115,10 +113,10 @@ func (s *SDKContextDestroySuite) TestProviderDestroy(c *check.C) {
 	var preCreateRoutinesNum int
 	routinesCount, _ = parseRoutines(getAllStack())
 	preCreateRoutinesNum = routinesCount
-	//log.Printf("preCreateRoutinesNum %v", preCreateRoutinesNum)
+	log.Printf("preCreateRoutinesNum %v", preCreateRoutinesNum)
 	provider, err := api.NewProviderAPIByConfig(configuration)
 	c.Assert(err, check.IsNil)
-	//log.Printf("providerAPI runningRoutinesNum %v", runtime.NumGoroutine())
+	log.Printf("providerAPI runningRoutinesNum %v", runtime.NumGoroutine())
 	provider.Destroy()
 	time.Sleep(5 * time.Second)
 	var lumberjackCount int
