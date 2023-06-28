000bac35ac12e563ea2de0292f3bb53b0b751682
chuntaojun <liaochuntao@live.com>
2023-06-28 16:58:59 +0800

    fix:修复监控上报组件不适配最新版本问题

--- test/loadbalance/discover_lb.go
+++ test/loadbalance/discover_lb.go
@@ -20,14 +20,19 @@ package loadbalance
 import (
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"os"
 
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/google/uuid"
 	"github.com/polarismesh/specification/source/go/api/v1/service_manage"
 	"google.golang.org/grpc"
 	"gopkg.in/check.v1"
 
+	"github.com/polarismesh/polaris-go/api"
 	"github.com/polarismesh/polaris-go/pkg/config"
+	"github.com/polarismesh/polaris-go/pkg/network"
 	"github.com/polarismesh/polaris-go/test/mock"
 	"github.com/polarismesh/polaris-go/test/util"
 )
@@ -81,4 +86,81 @@ func (t *InnerServiceLBTestingSuite) TearDownSuite(c *check.C) {
 	if util.DirExist(util.BackupDir) {
 		os.RemoveAll(util.BackupDir)
 	}
+}
+
+// TestConnManger 测试连接管理器
+func (t *InnerServiceLBTestingSuite) TestConnManger(c *check.C) {
+	service := &service_manage.Service{
+		Name:      &wrappers.StringValue{Value: config.ServerMonitorService},
+		Namespace: &wrappers.StringValue{Value: config.ServerNamespace},
+		Token:     &wrappers.StringValue{Value: uuid.New().String()},
+	}
+	var Instances []*service_manage.Instance
+
+	Instances = append(Instances, &service_manage.Instance{
+		Id:        &wrappers.StringValue{Value: uuid.New().String()},
+		Service:   &wrappers.StringValue{Value: config.ServerMonitorService},
+		Namespace: &wrappers.StringValue{Value: config.ServerNamespace},
+		Host:      &wrappers.StringValue{Value: "127.0.0.1"},
+		Port:      &wrappers.UInt32Value{Value: uint32(10030 + 1)},
+		Weight:    &wrappers.UInt32Value{Value: uint32(100)},
+		Metadata: map[string]string{
+			"protocol": "grpc",
+		},
+	})
+	Instances = append(Instances, &service_manage.Instance{
+		Id:        &wrappers.StringValue{Value: uuid.New().String()},
+		Service:   &wrappers.StringValue{Value: config.ServerMonitorService},
+		Namespace: &wrappers.StringValue{Value: config.ServerNamespace},
+		Host:      &wrappers.StringValue{Value: "127.0.0.1"},
+		Port:      &wrappers.UInt32Value{Value: uint32(10030 + 2)},
+		Weight:    &wrappers.UInt32Value{Value: uint32(300)},
+		Metadata: map[string]string{
+			"protocol": "grpc",
+		},
+	})
+	Instances = append(Instances, &service_manage.Instance{
+		Id:        &wrappers.StringValue{Value: uuid.New().String()},
+		Service:   &wrappers.StringValue{Value: config.ServerMonitorService},
+		Namespace: &wrappers.StringValue{Value: config.ServerNamespace},
+		Host:      &wrappers.StringValue{Value: "127.0.0.1"},
+		Port:      &wrappers.UInt32Value{Value: uint32(10030 + 3)},
+		Weight:    &wrappers.UInt32Value{Value: uint32(500)},
+		Metadata: map[string]string{
+			"protocol": "grpc",
+		},
+	})
+	fmt.Println(Instances)
+	t.mockServer.RegisterServiceInstances(service, Instances)
+	cfg := config.NewDefaultConfiguration([]string{fmt.Sprintf("%s:%d", lbIPAddr, lbPort)})
+	consumer, err := api.NewConsumerAPIByConfig(cfg)
+	c.Assert(err, check.IsNil)
+	defer consumer.Destroy()
+
+	mgr, err := network.NewConnectionManager(cfg, consumer.SDKContext().GetValueContext())
+	c.Assert(err, check.IsNil)
+
+	w100Count := 0
+	w300Count := 0
+	w500Count := 0
+
+	for i := 0; i < 10000; i++ {
+		_, inst, err := mgr.GetHashExpectedInstance(config.MonitorCluster, []byte(fmt.Sprintf("%d", i)))
+		c.Assert(err, check.IsNil)
+		weight := inst.GetWeight()
+		switch weight {
+		case 100:
+			w100Count++
+		case 300:
+			w300Count++
+		case 500:
+			w500Count++
+		}
+	}
+	fmt.Println(w100Count, w300Count, w500Count)
+	a1 := float64(w300Count) / float64(w100Count)
+	a2 := float64(w500Count) / float64(w100Count)
+	fmt.Println(a1, a2)
+	c.Assert(math.Abs(a1-3) < 0.8, check.Equals, true)
+	c.Assert(math.Abs(a2-5) < 0.8, check.Equals, true)
 }
