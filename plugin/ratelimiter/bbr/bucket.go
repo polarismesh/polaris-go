afa3a02a3577959cc9fb86bcc55e3f434cd2e9b7
wtifs <da_tou@126.com>
2023-09-01 14:51:48 +0800

    fix: 修正多条规则时，生效的优先级

--- plugin/ratelimiter/bbr/bucket.go
+++ plugin/ratelimiter/bbr/bucket.go
@@ -4,11 +4,12 @@ import (
 	aegislimiter "github.com/go-kratos/aegis/ratelimit"
 	"github.com/go-kratos/aegis/ratelimit/bbr"
 	"github.com/polarismesh/polaris-go/pkg/model"
-	"github.com/polarismesh/polaris-go/pkg/model/pb"
 	"github.com/polarismesh/polaris-go/pkg/plugin/ratelimiter"
 	apitraffic "github.com/polarismesh/specification/source/go/api/v1/traffic_manage"
+	"sort"
 )
 
+// BbrQuotaBucket 实现 BbrQuotaBucket 接口的结构体
 type BbrQuotaBucket struct {
 	aegislimiter.Limiter
 }
@@ -52,26 +53,38 @@ func createBbrLimiter(rule *apitraffic.Rule) *BbrQuotaBucket {
 	options := make([]bbr.Option, 0)
 
 	if amounts := rule.GetAmounts(); len(amounts) > 0 {
-		var amount = amounts[0]
-		var threshold = amount.GetMaxAmount().GetValue()
+		// 如果有多条规则：
+		// 1. 先按CPU阈值比较，阈值小的生效
+		// 2. 阈值相同时，按时间窗口比较，窗口小的生效
+		// 3. 窗口也相同时，按精度比较，精度大的生效
+		sort.Slice(amounts, func(i, j int) bool {
+			a, b := amounts[i], amounts[j]
+			threshold1, threshold2 := a.GetMaxAmount().GetValue(), b.GetMaxAmount().GetValue()
+			window1, window2 := a.GetValidDuration().AsDuration(), b.GetValidDuration().AsDuration()
+			precision1, precision2 := a.GetPrecision().GetValue(), b.GetPrecision().GetValue()
 
-		// 有多条规则时，只允许cpu使用率最低的规则生效
-		for i := 1; i < len(amounts); i++ {
-			if curThreshold := amounts[i].GetMaxAmount().GetValue(); curThreshold < threshold {
-				threshold = curThreshold
-				amount = amounts[i]
+			if threshold1 == threshold2 {
+				if window1 == window2 {
+					return precision1 > precision2
+				}
+				return window1 < window2
 			}
-		}
+			return threshold1 < threshold2
+		})
+
+		amount := amounts[0]
+
 		// CPU使用率阈值，默认80%
-		if threshold > 0 {
-			// bbr 的参数为 800‰ 的形式，要从 rule 中的百分号转到千分号，乘10
+		if threshold := amount.GetMaxAmount().GetValue(); threshold > 0 {
+			// bbr 的参数为 800‰ 的形式，需要从 rule 中的百分号转到千分号，乘10
 			options = append(options, bbr.WithCPUThreshold(int64(threshold*10)))
 		}
 		// 统计时间窗口，默认 10s
-		if window, _ := pb.ConvertDuration(amount.GetValidDuration()); window > 0 {
+		if window := amount.GetValidDuration().AsDuration(); window > 0 {
 			options = append(options, bbr.WithWindow(window))
 		}
 		// 观测时间窗口内 计数桶 的个数（控制滑动窗口精度），默认100个
+		// 如 window=1s, bucket=10 时，整个滑动窗口用来保存最近 1s 的采样数据，每个小的桶用来保存 100ms 的采样数据。当时间流动之后，过期的桶会自动被新桶的数据覆盖掉
 		if precision := amount.GetPrecision().GetValue(); precision > 0 {
 			options = append(options, bbr.WithBucket(int(precision)))
 		}
